810efa487af5adeb3ef1433bdb95b46ffe7f0003
uri <urasha-kms@yandex.ru>
2014-09-07 20:19:25 +1100

    Improve the README file

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -14,8 +14,6 @@ class SessionsController < ApplicationController
       user = User.find_by(name: params[:session][:name].downcase)
       if user && user.authenticate(params[:session][:password])
         # Sign the user in and redirect to the user's show page.
-        #sign_in user
-        #redirect_back_or user
         flash.now[:error] = 'Все хорошо'
 #      redirect_to(request.env["HTTP_REFERER"])
 
